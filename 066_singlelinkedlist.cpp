92a48d7ce071aab21da85f58f9de8f2befda7183
halimbagusalhakim <halsat132@gmail.com>
2024-04-26 14:39:26 +0700

    menampilkan dan menginput nim

--- 066_singlelinkedlist.cpp
+++ 066_singlelinkedlist.cpp
@@ -12,6 +12,14 @@ struct Node {
 Node* START = NULL;
 
 void addNode() {
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukan NIM : ";
+    cin >> nim;
+    
+
+   
     
 }
 
